2dfd8eb3e06e9e0832e6ffcf84b1decab2ff31a6
Admin3931 <Admin3931>
2024-03-07 19:18:43 -0600

    pushing changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
@@ -33,6 +34,23 @@ public class RobotContainer {
 
   private void configureBindings() {
     driverXbox.start().whileTrue(drivebase.zeroGyro());
+
+    // driverXbox.y().whileTrue(Commands.run(() -> {
+    //   drivebase.drive(drivebase.getTargetSpeeds(0,0, Rotation2d.fromDegrees(180)));
+    // }, drivebase));
+
+    // driverXbox.a().whileTrue(Commands.run(() -> {
+
+    //   double leftY = -MathUtil.applyDeadband(driverXbox.getLeftY(), 0.1);
+    //   double leftX = -MathUtil.applyDeadband(driverXbox.getLeftX(), 0.1);
+
+
+    //   drivebase.drive(drivebase.getTargetSpeeds(leftY,leftX, Rotation2d.fromDegrees(0)));
+    // }, drivebase));
+
+    driverXbox.y().whileTrue(drivebase.setHeadingHoldAngle(180));
+    driverXbox.a().whileTrue(drivebase.setHeadingHoldAngle(0));
+    
   }
 
   public Command getAutonomousCommand() {
--- src/main/java/frc/robot/subsystems/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/SwerveSubsystem.java
@@ -10,21 +10,28 @@ import java.io.File;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj.Filesystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import swervelib.parser.PIDFConfig;
 import swervelib.parser.SwerveParser;
 import swervelib.telemetry.SwerveDriveTelemetry;
 import swervelib.telemetry.SwerveDriveTelemetry.TelemetryVerbosity;
 import swervelib.SwerveDrive;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.util.Units;
 
 public class SwerveSubsystem extends SubsystemBase {
   SwerveDrive swerveDrive;
+  // double maximumSpeed = Units.feetToMeters(16.6);
+  double maximumSpeed = Units.feetToMeters(16.6);
+  double headingHoldAngle = 0;
 
   /** Creates a new SwerveSubsystem. */
   public SwerveSubsystem() {
     SwerveDriveTelemetry.verbosity = TelemetryVerbosity.HIGH;
     try {
-      double maximumSpeed = Units.feetToMeters(16.6);
       File swerveJsonDirectory = new File(Filesystem.getDeployDirectory(), "neo/swerve");
       swerveDrive = new SwerveParser(swerveJsonDirectory).createSwerveDrive(maximumSpeed);
     } catch (Exception e) {
@@ -32,6 +39,12 @@ public class SwerveSubsystem extends SubsystemBase {
     }
 
      swerveDrive.setHeadingCorrection(true);
+
+     SmartDashboard.putNumber("Heading", getHeading().getDegrees());
+  }
+
+  public void drive(ChassisSpeeds velocity) {
+    swerveDrive.drive(velocity);
   }
 
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY, DoubleSupplier headingX,
@@ -49,29 +62,89 @@ public class SwerveSubsystem extends SubsystemBase {
           swerveDrive.getOdometryHeading().getRadians(),
           swerveDrive.getMaximumVelocity()));
     });
+
+
+  //  SmartDashboard.put ("sdf", swerveDrive.swerveController.config.headingPIDF.createPIDController());
   }
+
+
+
+
+
 
   public Command driveCommand(DoubleSupplier translationX, DoubleSupplier translationY,
       DoubleSupplier angularRotationX) {
    
     return run(() -> {
+      double calculatedAngularRotation = 0;
+
+      if(Math.abs(angularRotationX.getAsDouble()) > 0.1) {
+        this.headingHoldAngle = swerveDrive.getOdometryHeading().getRadians();
+        // Slow things down.
+        calculatedAngularRotation = angularRotationX.getAsDouble() * swerveDrive.getMaximumAngularVelocity() * .75;
+      } else {
+        calculatedAngularRotation = swerveDrive.swerveController.headingCalculate(
+          swerveDrive.getOdometryHeading().getRadians(),
+          headingHoldAngle
+        );
+      }
+
       // Make the robot move
       swerveDrive.drive(new Translation2d(Math.pow(translationX.getAsDouble(), 3) * swerveDrive.getMaximumVelocity(),
           Math.pow(translationY.getAsDouble(), 3) * swerveDrive.getMaximumVelocity()),
-          Math.pow(angularRotationX.getAsDouble(), 3) * swerveDrive.getMaximumAngularVelocity(),
+          calculatedAngularRotation,
           true,
           false);
     });
   }
 
+
+
+
+
+
+
+
+
+
+  public Command setHeadingHoldAngle(double angleInDegree) {
+    return runOnce(() -> {
+      this.headingHoldAngle = Units.degreesToRadians(angleInDegree);
+    });
+  }
+
+
+
+
+
+  public ChassisSpeeds getTargetSpeeds(double xInput, double yInput, Rotation2d angle) {
+    xInput = Math.pow(xInput, 3);
+    yInput = Math.pow(yInput, 3);
+    return swerveDrive.swerveController.getTargetSpeeds(xInput,
+        yInput,
+        angle.getRadians(),
+        getHeading().getRadians(),
+        maximumSpeed);
+  }
+
+  public Rotation2d getHeading() {
+    return getPose().getRotation();
+  }
+
+  public Pose2d getPose() {
+    return swerveDrive.getPose();
+  }
+
   public Command zeroGyro() {
     return runOnce(() -> {
+      this.headingHoldAngle = 0;
       this.swerveDrive.zeroGyro();
     });
   }
 
   @Override
   public void periodic() {
+    SmartDashboard.putNumber("Heading", getHeading().getDegrees());
     // This method will be called once per scheduler run
   }
 }
